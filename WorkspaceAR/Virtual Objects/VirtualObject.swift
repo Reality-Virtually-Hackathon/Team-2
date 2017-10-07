4d715defe258a16c6edf63258c7be7420a4a595d
Xiao Ling <lingxiao@seas.upenn.edu>
2017-10-07 17:58:31 -0400

    mp stub

--- WorkspaceAR/Virtual Objects/VirtualObject.swift
+++ WorkspaceAR/Virtual Objects/VirtualObject.swift
@@ -20,7 +20,10 @@ class VirtualObject: SCNReferenceNode {
     /// Use average of recent virtual object distances to avoid rapid changes in object scale.
     private var recentVirtualObjectDistances = [Float]()
     
-    /// Resets the objects t smoothing.
+    // record path of motion imparted onto the object
+    var motionPath = [SCNVector3]()
+    
+    /// Resets the objects smoothing.
     func reset() {
         recentVirtualObjectDistances.removeAll()
     }
