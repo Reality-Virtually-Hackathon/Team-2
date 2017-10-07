8802fb51678449f3992d07ce0f78d01494a9c4a5
Joe Crotchett <joe.crotchett@mutualmobile.com>
2017-10-07 12:09:14 -0400

    code clean up

--- WorkspaceAR/Connectivity/ConnectivityManager.swift
+++ WorkspaceAR/Connectivity/ConnectivityManager.swift
@@ -10,8 +10,14 @@ import Foundation
 import MultipeerConnectivity
 
 protocol ConnectivityManagerDelegate {
-    func connectedDevicesChanged(manager : ConnectivityManager, connectedDevices: [String])
-    func dataReceived(manager: ConnectivityManager, data: Data)
+    
+    // Called when a peer has changed it's connection status
+    func connectedDevicesChanged(manager : ConnectivityManager,
+                                 connectedDevices: [String])
+    
+    // Called when data has been recieved from a peer
+    func dataReceived(manager: ConnectivityManager,
+                      data: Data)
 }
 
 class ConnectivityManager : NSObject
@@ -23,19 +29,27 @@ class ConnectivityManager : NSObject
     var delegate : ConnectivityManagerDelegate?
     
     lazy var session : MCSession = {
-        let session = MCSession(peer: self.myPeerId, securityIdentity: nil, encryptionPreference: .none)
+        let session = MCSession(peer: self.myPeerId,
+                                securityIdentity: nil,
+                                encryptionPreference: .none)
         session.delegate = self
         return session
     }()
     
-    override init() {
+    deinit {
+        if let advertiser = advertiser {
+            advertiser.stopAdvertisingPeer()
+        }
         
+        if let browser = browser {
+            browser.stopBrowsingForPeers()
+        }
     }
     
     func startAdvertising() {
         advertiser = MCNearbyServiceAdvertiser(peer: myPeerId,
-                                                      discoveryInfo: nil,
-                                                      serviceType: ServiceType)
+                                               discoveryInfo: nil,
+                                               serviceType: ServiceType)
         if let advertiser = advertiser {
             advertiser.delegate = self
             advertiser.startAdvertisingPeer()
@@ -54,7 +68,9 @@ class ConnectivityManager : NSObject
     func sendTestString() {
         if session.connectedPeers.count > 0 {
             do {
-                try self.session.send("test string".data(using: .utf8)!, toPeers: session.connectedPeers, with: .reliable)
+                try self.session.send("test string".data(using: .utf8)!,
+                                      toPeers: session.connectedPeers,
+                                      with: .reliable)
             }
             catch let error {
                 NSLog("%@", "Error for sending: \(error)")
@@ -63,9 +79,12 @@ class ConnectivityManager : NSObject
     }
 }
 
+//MARK: MCNearbyServiceAdvertiserDelegate
+
 extension ConnectivityManager: MCNearbyServiceAdvertiserDelegate {
     
-    func advertiser(_ advertiser: MCNearbyServiceAdvertiser, didNotStartAdvertisingPeer error: Error) {
+    func advertiser(_ advertiser: MCNearbyServiceAdvertiser,
+                    didNotStartAdvertisingPeer error: Error) {
         NSLog("%@", "didNotStartAdvertisingPeer: \(error)")
     }
     
@@ -78,47 +97,74 @@ extension ConnectivityManager: MCNearbyServiceAdvertiserDelegate {
     }
 }
 
+//MARK: MCNearbyServiceBrowserDelegate
+
 extension ConnectivityManager : MCNearbyServiceBrowserDelegate {
     
-    func browser(_ browser: MCNearbyServiceBrowser, didNotStartBrowsingForPeers error: Error) {
+    func browser(_ browser: MCNearbyServiceBrowser,
+                 didNotStartBrowsingForPeers error: Error) {
         NSLog("%@", "didNotStartBrowsingForPeers: \(error)")
     }
     
-    func browser(_ browser: MCNearbyServiceBrowser, foundPeer peerID: MCPeerID, withDiscoveryInfo info: [String : String]?) {
+    func browser(_ browser: MCNearbyServiceBrowser,
+                 foundPeer peerID: MCPeerID,
+                 withDiscoveryInfo info: [String : String]?) {
         NSLog("%@", "foundPeer: \(peerID)")
         NSLog("%@", "invitePeer: \(peerID)")
-        browser.invitePeer(peerID, to: self.session, withContext: nil, timeout: 10)
+        browser.invitePeer(peerID,
+                           to: self.session,
+                           withContext: nil,
+                           timeout: 10)
     }
     
-    func browser(_ browser: MCNearbyServiceBrowser, lostPeer peerID: MCPeerID) {
+    func browser(_ browser: MCNearbyServiceBrowser,
+                 lostPeer peerID: MCPeerID) {
         NSLog("%@", "lostPeer: \(peerID)")
     }
 }
 
+//MARK: MCSessionDelegate
+
 extension ConnectivityManager : MCSessionDelegate {
     
-    func session(_ session: MCSession, peer peerID: MCPeerID, didChange state: MCSessionState) {
+    func session(_ session: MCSession,
+                 peer peerID: MCPeerID,
+                 didChange state: MCSessionState) {
         NSLog("%@", "peer \(peerID) didChangeState: \(state)")
-        self.delegate?.connectedDevicesChanged(manager: self, connectedDevices:
-            session.connectedPeers.map{$0.displayName})
+        self.delegate?.connectedDevicesChanged(manager: self,
+                                               connectedDevices: session.connectedPeers.map{$0.displayName})
     }
     
-    func session(_ session: MCSession, didReceive data: Data, fromPeer peerID: MCPeerID) {
+    func session(_ session: MCSession,
+                 didReceive data: Data,
+                 fromPeer peerID: MCPeerID) {
         NSLog("%@", "didReceiveData: \(data)")
-        if let message = String(data: data, encoding: .utf8) {
-            self.delegate?.dataReceived(manager: self, data: message.data(using: .utf8)!)
+        if let message = String(data: data,
+                                encoding: .utf8) {
+            self.delegate?.dataReceived(manager: self,
+                                        data: message.data(using: .utf8)!)
         }
     }
     
-    func session(_ session: MCSession, didReceive stream: InputStream, withName streamName: String, fromPeer peerID: MCPeerID) {
+    func session(_ session:
+                 MCSession, didReceive stream:
+                 InputStream, withName streamName:
+                 String, fromPeer peerID: MCPeerID) {
         assert(true, "not impelemented")
     }
     
-    func session(_ session: MCSession, didStartReceivingResourceWithName resourceName: String, fromPeer peerID: MCPeerID, with progress: Progress) {
+    func session(_ session: MCSession,
+                 didStartReceivingResourceWithName resourceName: String,
+                 fromPeer peerID: MCPeerID,
+                 with progress: Progress) {
         assert(true, "not impelemented")
     }
     
-    func session(_ session: MCSession, didFinishReceivingResourceWithName resourceName: String, fromPeer peerID: MCPeerID, at localURL: URL?, withError error: Error?) {
+    func session(_ session: MCSession,
+                 didFinishReceivingResourceWithName resourceName: String,
+                 fromPeer peerID: MCPeerID,
+                 at localURL: URL?,
+                 withError error: Error?) {
         assert(true, "not impelemented")
     }
 }
